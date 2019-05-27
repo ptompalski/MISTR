a072fb8447c8bcbd46c71e61005f11fbcb7b77f0
Tompalski <ptrtmp@ead.ubc.ca>
2019-05-27 14:27:19 -0700

    Added function species2unit

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,31 @@
 is.empty <- function(x) {
   length(x) == 0
 }
+
+
+# function to extract species composition from strings commonly used in Ontario
+# eg: "PW 60PR 40"
+# currently this function is limited to only the above species composition format
+
+species_comp_string2df <- function(sp_comp_string) {
+  sp_comp_string <- stringr::str_squish(sp_comp_string)
+
+  a1 <- stringr::str_split(string = sp_comp_string, pattern = " ")
+
+  # text
+  a1.text <- stringr::str_extract(a1[[1]],"[A-Z]{2}")
+  # digits
+  a1.digits <- stringr::str_extract(a1[[1]],"[[:digit:]]+")
+
+  #correct species codes
+  a1.text <- toupper(a1.text)
+  #a1.text <- gsub(x = a1.text, pattern = "PT", replacement = "AT")
+
+  result <- data.frame(species_code = a1.text[!is.na(a1.text)],
+                       species_proportion = as.numeric(a1.digits[!is.na(a1.digits)]) / 100)
+  return(result)
+
+}
+
+
+
